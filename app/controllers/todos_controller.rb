426579afa53e5948e8bdcf3c7b59753cfee31683
Veronica Saldana <v.saldana.c@gmail.com>
2020-02-04 21:20:01 -0300

    add methods show, edit, update and destroy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,37 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end
+
+    def create
+        @todo = Todo.new(todos_params)
+        if @todo.save
+            redirect_to todos_path
+        end
+    end
+
+    def show
+        @todo = Todo.find(params[:id])
+    end
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todos_params)
+        redirect_to todos_path
+    end
+    
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        redirect_to todos_path
+    end
+
+private
+    def todos_params
+        params.require(:todo).permit(:description, :completed)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/todos', to: 'todos#index'
   get '/todos/new', to: 'todos#new'
+  post '/todos', to: 'todos#create'
+  get '/todos/:id', to: 'todos#show', as: 'todo'
+  get '/todos/:id/edit', to: 'todos#edit', as: 'todos/edit'
+  patch '/todos/:id', to: 'todos#update'
+  delete '/todos/:id', to: 'todos#destroy', as: 'todos/delete'
+  get '/todos/:id/complete', to: 'todos#complete'
 end
